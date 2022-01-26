6633e49163154c18ea246601c91d69dc4166b5ae
kawulaed <ekawula@gmail.com>
2022-01-25 17:49:59 -0800

    commit tree 3d27dcaafbe60ad884af536fa3494251e35ee442

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+    let newcat = [...cats];
+    newcat.push(name);
+    return newcat;
+}
+function prependCat(name) {
+    let newcat = [...cats];
+    newcat.unshift(name);
+    return newcat;
+}
+function removeLastCat() {
+    let newcat = [...cats];
+    newcat.pop()
+    return newcat
+}
+function removeFirstCat() {
+    let newcat = [...cats];
+    newcat.shift();
+    return newcat
+}
